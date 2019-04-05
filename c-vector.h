3cec380357ca5a21b3e22faaca840db4a18697e9
Joshua Weinstein <jweinstein@splunk.com>
2019-04-05 16:07:02 -0700

    commas

--- c-vector.h
+++ c-vector.h
@@ -3,18 +3,27 @@
 
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * Small memory pool struct,
+ * basically keeps track of available memory region and
+ * the total size.
+ */
 typedef struct __c_mem_pool {
 	void* current;
 	void* end;
 	size_t size;
 } mem_pool_t;
-
+/**
+ * represents a single item inside the larger vector struct.
+ */
 typedef struct _c_vector_item {
 	size_t size;
 	void* data;
 } c_vector_item_t;
-
+/**
+ * A heterogenous vector data structure, meant to have constant time
+ * random access, pushing, and popping. 
+ */
 typedef struct _c_vector {
 	mem_pool_t* memory;
 	size_t len;
@@ -24,8 +33,18 @@ typedef struct _c_vector {
 
 
 /*PUBLIC API*/
+/**
+ * Creates a new vector with a default mem pool and item capacity.
+ * Can be destroyed by calling c_vector_del().
+ */
 extern c_vector_t* c_vector_new(void);
 /*PUBLIC API*/
+/**
+ * This pushes a new item onto the end of the vector.
+ * Calling this does not incur a new memory allocation,
+ * new memory is onyl allocated when the item length 
+ * reaches capacity, or the memory pool becomes used up.
+ */
 extern void c_vector_push(c_vector_t* vec, void* item, size_t item_size);
 /**
  * PUBLIC API
